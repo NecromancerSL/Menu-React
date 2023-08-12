2184c7af31c3e15b4413c22ffc079dd435c2d0cd
Gustavo Nunes <dsnunes.g42@outlook.com>
2023-08-12 18:32:27 -0300

    Adicionando Buscador e botões de filtro, ainda sem funcionalidade

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,18 +1,29 @@
-import styles from './Menu.module.scss';
-import { ReactComponent as Logo} from 'assets/logo.svg'
+import styles from "./Menu.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./search";
+import { useState } from "react";
+import Filter from "./filter";
 
-
-export default function Menu(){
-    return(
+export default function Menu() {
+    const [busca,setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
+    return (
         <main>
-            <nav className={styles.menu}>
-                <Logo />
-                <header className={styles.header}>
-                    <div className={styles.header__text}>
-                        Ensinando como um italiano segura uma arma e fazendo deliciosas massas
-                    </div>
-                </header>
-            </nav>
+        <nav className={styles.menu}>
+            <Logo />
+        </nav>
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+            Ensinando como segurar uma arma e fazendo deliciosas massas
+            </div>
+        </header>
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__titulo}>Menu</h3>
+            <Buscador busca={busca} setBusca={setBusca}/>
+            <div className={styles.cardapio__filtros}>
+                <Filter filtro={filtro} setFiltro={setFiltro}/>
+            </div>
+        </section>
         </main>
-    );
+    )
 }
